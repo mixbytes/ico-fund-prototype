acc162631e2bc518933553f513871bb102f4f51d
Eenae <z-line@my.com>
2017-10-27 11:01:44 +0300

    basic voting

--- contracts/DAOFund.sol
+++ contracts/DAOFund.sol
@@ -6,6 +6,12 @@ import './IDAOToken.sol';
 /// @title ICO fund controlled by the investors
 contract DAOFund {
 
+    enum ApprovalState {
+        NotVoted,
+        Approval,
+        Disapproval
+    }
+
     // Decision point description for the DAO
     struct KeyPoint {
         // duration of the period which is evaluated at this point
@@ -17,13 +23,32 @@ contract DAOFund {
 
     // Dynamic state of a KeyPoint
     struct KeyPointState {
+        bool processed;
+
         // true iff decision was made to further finance the project
         bool success;
+
+        uint votingEndTime;
+
+        uint approvalVotes;
+        uint disapprovalVotes;
+        mapping(address => ApprovalState) approvalState;
     }
 
 
     // event fired when the DAO comes to conclusion about a KeyPoint
     event KeyPointResolved(uint keyPointIndex, bool success);
+
+
+    modifier onlyActive {
+        require(isActive());
+        _;
+    }
+
+    modifier onlyTokenHolder {
+        require(m_token.balanceOf(msg.sender) > 0);
+        _;
+    }
 
 
     // PUBLIC interface
@@ -38,14 +63,34 @@ contract DAOFund {
         validateKeyPoints();
 
         // first tranche after the ICO
-        m_keyPointState.push(KeyPointState({success: true}));
+        m_keyPointState.push(createKeyPointState(now));
+        m_keyPointState[0].processed = true;
+        m_keyPointState[0].success = true;
         KeyPointResolved(0, true);
+        initNextKeyPoint();
+
+        assert(isActive());
+    }
+
+    function approveKeyPoint(bool approval) external onlyActive onlyTokenHolder {
+        KeyPointState storage state = getCurrentKeyPointState();
+        require(now < state.votingEndTime);
+        require(state.approvalState[msg.sender] == ApprovalState.NotVoted);
+
+        if (approval) {
+            state.approvalState[msg.sender] = ApprovalState.Approval;
+            state.approvalVotes += m_token.balanceOf(msg.sender);
+        } else {
+            state.approvalState[msg.sender] = ApprovalState.Disapproval;
+            state.disapprovalVotes += m_token.balanceOf(msg.sender);
+        }
     }
 
 
     // INTERNALS
 
     function validateKeyPoints() private constant {
+        assert(m_keyPoints.length > 1);
         uint fundsTotal;
         for (uint i = 0; i < m_keyPoints.length; i++) {
             KeyPoint storage keyPoint = m_keyPoints[i];
@@ -54,6 +99,35 @@ contract DAOFund {
             fundsTotal += keyPoint.fundsShare;
         }
         assert(100 == fundsTotal);
+    }
+
+    function isActive() private constant returns (bool) {
+        assert(m_keyPoints.length >= m_keyPointState.length);
+        return m_keyPoints.length > m_keyPointState.length
+                || m_keyPoints.length == m_keyPointState.length && !(m_keyPointState[m_keyPointState.length - 1].processed);
+    }
+
+    function initNextKeyPoint() private {
+        assert(m_keyPoints.length > m_keyPointState.length);
+
+        KeyPoint storage keyPoint = m_keyPoints[m_keyPointState.length];
+        m_keyPointState.push(createKeyPointState(now + keyPoint.duration));
+    }
+
+
+    function getCurrentKeyPoint() private constant returns (KeyPoint storage) {
+        assert(isActive());
+        return m_keyPoints[m_keyPointState.length - 1];
+    }
+
+    function getCurrentKeyPointState() private constant returns (KeyPointState storage) {
+        assert(isActive());
+        return m_keyPointState[m_keyPointState.length - 1];
+    }
+
+    function createKeyPointState(uint votingEndTime) private constant returns (KeyPointState memory) {
+        return KeyPointState({processed: false, success: false,
+                votingEndTime: votingEndTime, approvalVotes: 0, disapprovalVotes: 0});
     }
 
 
